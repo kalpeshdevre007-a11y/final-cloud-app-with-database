5dd6b1f235f29bdfe53664fa6e44dffeb51b1601
kalpeshdevre007-a11y <kalpeshdevre007@gmail.com>
2026-04-18 19:15:17 +0530

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
     path(route='', view=views.index, name='index'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
     path('<int:course_id>/course_details/', views.course_details, name='course_details'),
     path('<int:course_id>/submit/', views.submit, name='submit'),
